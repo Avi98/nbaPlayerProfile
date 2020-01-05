1387585ef874a67d8af23609b69c8aa45499695c
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-01-06 00:17:02 +0530

    refactoered

--- src/components/tabs/TabsContainer.tsx
+++ src/components/tabs/TabsContainer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 export interface TabsContainerProps {
     value: number,
@@ -7,14 +8,45 @@ export interface TabsContainerProps {
     // todo
     children: any
 }
+export type value = {
+    value: number,
+}
+type Tab = {
+    checked?: boolean,
+    label: string,
+    id: string //TODO: should be unique,
+    onClick?: React.MouseEventHandler
+} & value
 
+
+type Label = {
+    active: boolean;
+}
+const Label = styled.button<Label>`
+        font-weight: 800;
+        font-size: 0.5rem;
+        margin-left: 1rem;
+        text-transform: uppercase;
+        padding: 0.8rem  1.0rem;
+        border-radius: 10px;
+        font-family:sans-serif;
+        background: ${p => p.active ? `var(--white)` : 'none'};
+        border:none;
+        outline:none;
+`
+export const Tab: React.SFC<Tab> = ({ checked = false, value, label, id, ...rest}: Tab) => {
+    return (
+        <>
+            <Label active={checked} {...rest}>{label}</Label>
+        </>
+    )
+}
 const TabsContainer: React.SFC<TabsContainerProps> = ({
     value,
     onChange,
     container: Container = 'div',
     children: childProp
 }) => {
-    console.log('onChange', onChange)
     const children = React.Children.map(childProp, (child) => {
         const childValue = child.props.value
         return React.cloneElement(child, { onClick:onChange, value: childValue })
--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,23 +1,8 @@
 import React from 'react'
 import styled from "styled-components/macro";
-import TabsContainer from './TabsContainer';
+import TabsContainer, { value, Tab} from './TabsContainer';
 
 export interface TabsProps {
-}
-
-type value = {
-    value: number,
-}
-type Tab = {
-    checked?: boolean,
-    label: string,
-    id: string //TODO: should be unique,
-    onClick?: React.MouseEventHandler
-} & value
-
-
-type Label = {
-    active: boolean;
 }
 
 const Container = styled.div<value>`
@@ -26,25 +11,6 @@ const Container = styled.div<value>`
     }
 `
 
-const Label = styled.button<Label>`
-        font-weight: 800;
-        font-size: 0.5rem;
-        margin-left: 1rem;
-        text-transform: uppercase;
-        padding: 0.8rem  1.0rem;
-        border-radius: 10px;
-        font-family:sans-serif;
-        background: ${p => p.active ? `var(--white)` : 'none'};
-        border:none;
-`
-
-const Tab: React.SFC<Tab> = ({ checked = false, value, label, id, ...rest}: Tab) => {
-    return (
-        <>
-            <Label active={checked} {...rest}>{label}</Label>
-        </>
-    )
-}
 const Tabs: React.SFC<TabsProps> = () => {
     return (
         <TabsContainer onChange={(e) => console.log('esdfsdg', e)} value={1} container={Container}>
